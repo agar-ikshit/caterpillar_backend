2fbcd38c28cef9dc4b40842cdee628bb383c39f1
agar-ikshit <ikshitagarwa@gmail.com>
2024-08-09 21:40:37 +0530

    getting data

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json()); // To parse JSON request bodies
+app.use(express.json()); 
 
-// Simple GET route
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// POST route to receive data from Flutter app
+
 app.post('/users', (req, res) => {
-  const userData = req.body; // Extract the data sent from the Flutter app
+  const userData = req.body; 
   console.log('Received user data:', userData);
 
-  // Respond back to the Flutter app
   res.status(200).json({ message: 'User data received successfully', data: userData });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

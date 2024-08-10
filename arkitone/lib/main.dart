5b159049aaeff5e11d280a82f84dd46affdb0095
Ironthor200r <131636311+Ironthor200r@users.noreply.github.com>
2024-08-11 03:27:44 +0530

    pushing pages

--- arkitone/lib/main.dart
+++ arkitone/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:arkitone/ar_screen.dart';
+import 'package:arkitone/arpage.dart';
+import 'package:arkitone/clientpage.dart';
+import 'package:arkitone/health.dart';
+import 'package:arkitone/homelander.dart';
 import 'package:arkitone/homepage.dart';
+import 'package:arkitone/login.dart';
+import 'package:arkitone/start.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +16,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Voice Recognition Form',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
-      home: VoiceForm() ,
+      ),  
+      home: homepage(),
     );
   }
 }
